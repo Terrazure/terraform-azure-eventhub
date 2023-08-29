1a5c762d46980e9edc8801e1c83deda44f46726d
Pinaki Kundu <pinaki.kundu@ses.com>
2023-08-30 01:50:27 +0530

    Fix CICD issue

--- eventhub.tf
+++ eventhub.tf
@@ -1,9 +1,9 @@
 resource "azurerm_eventhub" "this" {
-  for_each = var.hubs
+  count = length(var.hubs)
 
   name                = module.naming.eventhub.name
   namespace_name      = azurerm_eventhub_namespace.ehn.name
   resource_group_name = var.resource_group_name
-  partition_count     = each.value.partitions
-  message_retention   = each.value.message_retention
+  partition_count     = var.hubs[count.index].partitions
+  message_retention   = var.hubs[count.index].message_retention
 }
