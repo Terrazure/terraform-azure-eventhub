a162a8fddcaba2406124e7b4f02e13b7098fe79f
PinakiKundu <pinakispecial@gmail.com>
2025-06-02 04:42:41 +0530

    Fix formatting issues

--- eventhub.tf
+++ eventhub.tf
@@ -1,8 +1,8 @@
 resource "azurerm_eventhub" "this" {
   count = length(var.hubs)
 
-  name                = module.naming.eventhub.name
-  namespace_id        = azurerm_eventhub_namespace.ehn.id
-  partition_count     = var.hubs[count.index].partitions
-  message_retention   = var.hubs[count.index].message_retention
+  name              = module.naming.eventhub.name
+  namespace_id      = azurerm_eventhub_namespace.ehn.id
+  partition_count   = var.hubs[count.index].partitions
+  message_retention = var.hubs[count.index].message_retention
 }
